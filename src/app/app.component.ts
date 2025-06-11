44b4a69868e86b9dd52cb8de88dd22516ca2429a
Timon.D3v <fiedlertimon@gmail.com>
2025-06-11 02:20:34 +0200

    Removed boilerplate Added basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    imports: [RouterOutlet, HeaderComponent, NavigationComponent, FooterComponent],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'zurich-meets-tanzania-angular';
 }
