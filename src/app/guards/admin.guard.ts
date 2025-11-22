ad5b8707b1e43215e3da8d4f4c6fec593d662d4b
Timon.dev <fiedlertimon@gmail.com>
2025-11-22 17:19:57 +0100

    Added the auth guards Improved some styles Fixed a wrong link url Added rel to the imprint links

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
-export const adminGuard: CanActivateFn = (route, state) => {
-    return true;
+export const adminGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if (authService.isLoggedIn() && authService.user()?.type === "admin") {
+        return true;
+    }
+
+    router.navigate(["/login"]);
+    return false;
 };
--- src/app/guards/not-auth.guard.ts
+++ src/app/guards/not-auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
-export const notAuthGuard: CanActivateFn = (route, state) => {
+export const notAuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if (authService.isLoggedIn()) {
+        router.navigate(["/"]);
+        return false;
+    }
+
     return true;
 };
