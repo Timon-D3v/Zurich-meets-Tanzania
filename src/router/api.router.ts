80b45b7e154e2a4b60bf9b67ef3d07de15a3e69e
Timon.D3v <fiedlertimon@gmail.com>
2025-12-15 17:01:54 +0100

    Changed the rate limit middleware to only server under /api and not every route

--- src/router/api.router.ts
+++ src/router/api.router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import rateLimit from "express-rate-limit";
 
 import authRouter from "./auth.api.router";
 import blogRouter from "./blog.api.router";
@@ -8,6 +9,18 @@ import newsletterRouter from "./newsletter.router";
 
 // Router Serves under /api
 const router = Router();
+
+/**
+ * Allow an average max of 5 requests per second per minute.
+ * This has the same effect as a rate limit of 5 requests per second but is
+ * a little more flexible if requests stack while the site loads.
+ */
+router.use(
+    rateLimit({
+        limit: 5 * 60,
+        windowMs: 1000 * 60,
+    }),
+);
 
 router.use("/auth", authRouter);
 router.use("/blog", blogRouter);
--- src/router/root.router.ts
+++ src/router/root.router.ts
@@ -1,22 +1,9 @@
 import { Router } from "express";
-import rateLimit from "express-rate-limit";
 
 import apiRouter from "./api.router";
 
 // Router Serves under /
 const router = Router();
-
-/**
- * Allow an average max of 5 requests per second per minute.
- * This has the same effect as a rate limit of 5 requests per second but is
- * a little more flexible if requests stack while the site loads.
- */
-router.use(
-    rateLimit({
-        limit: 5 * 60,
-        windowMs: 1000 * 60,
-    }),
-);
 
 router.use("/api", apiRouter);
 
