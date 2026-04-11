efb1de6439cfe79a86f8fc09ccc65c25bdc99ae7
Timon.dev <fiedlertimon@gmail.com>
2026-04-11 21:03:07 +0200

    Started with the basic design of the profile page

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, effect, inject, signal } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { PublicUser } from "../..";
 
 @Component({
     selector: "app-account",
@@ -6,4 +8,42 @@ import { Component } from "@angular/core";
     templateUrl: "./account.component.html",
     styleUrl: "./account.component.scss",
 })
-export class AccountComponent {}
+export class AccountComponent {
+    user = signal<PublicUser>({
+        email: "Laden...",
+        firstName: "Laden...",
+        lastName: "Laden...",
+        phone: "Laden...",
+        address: "Laden...",
+        type: "user",
+        picture: "/svg/personal.svg",
+    });
+
+    bannerId = signal<number>(this.getRandomInt(1, 10));
+
+    private authService = inject(AuthService);
+
+    private _updateUserObject = effect(() => {
+        const userObject = this.authService.user();
+
+        if (userObject === null) {
+            this.user.set({
+                email: "Laden...",
+                firstName: "Laden...",
+                lastName: "Laden...",
+                phone: "Laden...",
+                address: "Laden...",
+                type: "user",
+                picture: "/svg/personal.svg",
+            });
+        } else {
+            this.user.set(userObject);
+        }
+    });
+
+    getRandomInt(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
